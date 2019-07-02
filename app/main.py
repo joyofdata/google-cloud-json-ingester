1c79ef2e0e59d97de383a405d251840a32d7071f
Raffael Vogler <mail@raffael-vogler.de>
2019-07-02 12:49:17 +0200

    init

---
+++ app/main.py
@@ -0,0 +1,16 @@
+import logging
+
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+@app.route('/dummy', methods=['GET'])
+def dummy():
+    val = request.args.get('val')
+    if val is None:
+      return 'Value is missing.', 400
+    return val + "_x", 200
+
+if __name__ == '__main__':
+app.run(host='127.0.0.1', port=8080, debug=True)
