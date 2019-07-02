3dd36d3904921d4170e0466d799e9aad35f9198f
Raffael Vogler <mail@raffael-vogler.de>
2019-07-02 14:31:21 +0200

    deployment annotation

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env bash
 
+echo ""
+echo ">>>>> replace project ID in YAML files ..."
+echo ""
 sed "s/{{PROJECT_ID}}/$1/g" ./openapi_template.yaml > ./openapi.yaml
 sed "s/{{PROJECT_ID}}/$1/g" ./app/app_template.yaml > ./app/app.yaml
 
+echo ""
+echo ">>>>> deploy API ..."
+echo ""
 gcloud endpoints services deploy "./openapi.yaml"
+
 # gcloud app create --region="europe-west3"
+
+echo ""
+echo ">>>>> deploy app ..."
+echo ""
 gcloud -q app deploy "./app/app.yaml"
