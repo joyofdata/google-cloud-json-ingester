c866af7a88013be12a559ff1b35de470697f8e97
Raffael Vogler <mail@raffael-vogler.de>
2019-07-03 20:40:58 +0200

    set region and create bucket

--- deploy.sh
+++ deploy.sh
@@ -7,11 +7,19 @@ sed "s/{{PROJECT_ID}}/$1/g" ./openapi_template.yaml > ./openapi.yaml
 sed "s/{{PROJECT_ID}}/$1/g" ./app/app_template.yaml > ./app/app.yaml
 
 echo ""
+echo ">>>>> set region to Frankfurt if (still) possible ..."
+echo ""
+gcloud app create --region="europe-west3"
+
+echo ""
+echo ">>>>> create bucket for raw json data ..."
+echo ""
+gsutil mb gs://raw-json-data-l3z0dbnsd39k/
+
+echo ""
 echo ">>>>> deploy API ..."
 echo ""
 gcloud endpoints services deploy "./openapi.yaml"
-
-# gcloud app create --region="europe-west3"
 
 echo ""
 echo ">>>>> deploy app ..."
