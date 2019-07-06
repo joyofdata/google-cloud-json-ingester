10421be02c13fb22c4a3e423d514b13607d8cf54
Raffael Vogler <mail@raffael-vogler.de>
2019-07-06 20:37:26 +0200

    handle bigtable instance id (table creation suspended)

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 
 # $1: google app engine project-id
 # $2: name of bucket for raw json data
+# $3: id of bigtable instance
 
 announcement () {
     echo ""
@@ -9,11 +10,12 @@ announcement () {
     echo ""
 }
 
-announcement "replace project ID in YAML files"
+announcement "set config values in YAML files"
 sed "s/{{PROJECT_ID}}/$1/g" ./openapi_template.yaml > ./openapi.yaml
 sed "
     s/{{PROJECT_ID}}/$1/g;
     s/{{BUCKET_NAME_FOR_RAW_DATA}}/$2/g;
+    s/{{BIGTABLE_INSTANCE_ID}}/$3/g;
     " ./app/app_template.yaml > ./app/app.yaml
 
 announcement "set region to Frankfurt if (still) possible"
@@ -21,6 +23,10 @@ gcloud app create --region="europe-west3"
 
 announcement "create bucket for raw json data if it doesn't exist yet"
 gsutil mb gs://$2/
+
+#announcement "create table with column family"
+#cbt createtable data
+#cbt createfamily data statistics
 
 announcement "deploy API"
 gcloud endpoints services deploy "./openapi.yaml"
