bbfe021fb6d18d35d4e690f9da4dcabefd7dac34
Raffael Vogler <mail@raffael-vogler.de>
2019-07-13 01:02:12 +0200

    the deploy plan completed

--- deploy.py
+++ deploy.py
@@ -1,21 +1,72 @@
 import yaml
+import os
+
 import deploy_utils as du
+
 
 config = yaml.load(open("deploy.yaml", "r"))
 
-project_id = config["project-id"]
+f_log = open("./deploy.log", "w+")
 
-
-bucket = config["cloud-storage"]["raw-data-bucket"]
-du.create_bucket(
-    name=bucket["name"],
-    location=bucket["location"]
+os.environ.setdefault(
+    key="GOOGLE_APPLICATION_CREDENTIALS",
+    value=config["project"]["google-application-credentials"]
 )
 
 
-datasets = config["bigquery"]["datasets"]
-du.create_datasets(datasets=datasets)
+du.replace_tokens_in_config_files(
+    map_token_value=[
+        ("PROJECT_ID", config["project"]["id"]),
+        ("BUCKET_NAME_FOR_RAW_DATA", config["cloud-storage"]["raw-data-bucket"]["name"]),
+        ("PUBSUB_NAME_CLOUD_STORAGE", config["pubsub"]["topics"][0]["name"])
+    ],
+    map_template_to_config=[
+        (config["project"]["config-templates"]["app-yaml"],    config["project"]["config-files"]["app-yaml"]),
+        (config["project"]["config-templates"]["openapi-yaml"],config["project"]["config-files"]["openapi-yaml"])
+    ],
+    f_log=f_log
+)
 
 
-tables = config["bigquery"]["tables"]
-du.create_tables(project_id=project_id, tables=tables)
+bucket = config["cloud-storage"]["raw-data-bucket"]
+du.create_storage_bucket(
+    name=bucket["name"],
+    location=bucket["location"],
+    f_log=f_log
+)
+
+du.create_bigquery_datasets(
+    datasets=config["bigquery"]["datasets"],
+    f_log=f_log
+)
+
+du.create_bigquery_tables(
+    project_id=config["project"]["id"],
+    tables=config["bigquery"]["tables"],
+    f_log=f_log
+)
+
+du.create_pubsub_topics(
+    project_id=config["project"]["id"],
+    topics=config["pubsub"]["topics"],
+    f_log=f_log
+)
+
+du.deploy_cloud_functions(
+    cloud_functions=config["cloud-functions"],
+    cwd=os.getcwd(),
+    f_log=f_log
+)
+
+du.deploy_endpoints_api(
+    openapi_yaml=config["project"]["config-files"]["openapi-yaml"],
+    cwd=os.getcwd(),
+    f_log=f_log
+)
+
+du.deploy_appengine_app(
+    app_yaml=config["project"]["config-files"]["app-yaml"],
+    region=config["project"]["region"],
+    cwd=os.getcwd(),
+    f_log=f_log
+)
