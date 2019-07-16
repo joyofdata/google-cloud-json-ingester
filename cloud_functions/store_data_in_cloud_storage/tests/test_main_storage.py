58e3e01553911466e2d564bdc7b68c1e515a9c07
Raffael Vogler <mail@raffael-vogler.de>
2019-07-16 19:55:09 +0200

    the devil is in the detail

--- cloud_functions/store_data_in_cloud_storage/tests/test_main_storage.py
+++ cloud_functions/store_data_in_cloud_storage/tests/test_main_storage.py
@@ -35,26 +35,27 @@ def test_store_data_in_cloud_storage():
     data = {
         "bucket_name": b.name,
         "object_name": "test.json",
-        "object_data": '{"time_stamp": "2019-05-02T06:00:00-04:00", "data": [1.2, 2.3, 3.4,4.5,5.6]}'
+        "object_data": '{"time_stamp": "2019-05-02T06:00:00-04:00", "data": [1.2, 2.3, 3.4,4.5,5.6]}',
+        "prepend_random_string_to_object_name": True
     }
 
-    data_packed = base64.b64encode(json.dumps(data).encode('utf-8'))
+    data_packed = {"data": base64.b64encode(json.dumps(data).encode('utf-8'))}
 
     m.store_data_in_cloud_storage(data_packed, None)
 
     time.sleep(1)
 
     try:
-        bucket = client.get_bucket(b.name)
-        blob = bucket.get_blob("test.json")
+        b = client.get_bucket(b.name)
+        blob_name = [blob.name for blob in b.list_blobs()][0]
+        blob = b.get_blob(blob_name)
         content = blob.download_as_string()
     except:
         raise Exception("File 'test.json' not available from bucket '{}'.".format(b.name))
 
-    assert content == data["object_data"]
+    assert content.decode("utf-8") == data["object_data"]
 
-    #b = client.get_bucket(b.name)
-    #b.delete_blobs(blobs=b.list_blobs())
-    #b.delete()
+    b.delete_blobs(blobs=b.list_blobs())
+    b.delete()
 
     return
